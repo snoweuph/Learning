906c0a2bf2bde14d53d9581cae5ef18a5a37fca0
Snoweuph <domsaeume@gmail.com>
2022-02-28 23:13:20 +0100

    basic layout and styling

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AddTask from "./components/AddTask.vue";
+import TaskTable from "./components/TaskTable.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <h1>Vue.js Todo App</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <AddTask />
+    <TaskTable />
   </main>
 </template>
 
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
+@import "./assets/utility.css";
+@import "./assets/table.css";
 
 #app {
-  max-width: 1280px;
+  max-width: 60rem;
   margin: 0 auto;
   padding: 2rem;
-
+  display: grid;
   font-weight: normal;
 }
-
-header {
-  line-height: 1.5;
-}
-
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
 }
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+h1 {
+  text-align: center;
 }
 </style>
---
+++ src/components/TaskTable.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          name: "Placeholder Name 1",
+          status: "to-do",
+        },
+        {
+          name: "Placeholder Name 2",
+          status: "done",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<template>
+  <table class="task-table">
+    <thead>
+      <tr>
+        <th scope="col">Task</th>
+        <th scope="col">Status</th>
+        <th scope="col" class="text-center table-icon">#</th>
+        <th scope="col" class="text-center table-icon">#</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td>{{ task.name }}</td>
+        <td>{{ task.status }}</td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-pen"></span>
+          </div>
+        </td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-trash"></span>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+
+<style>
+.task-table {
+  width: 100%;
+  margin: 1rem auto;
+}
+</style>
