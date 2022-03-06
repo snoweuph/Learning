6137d38cf51bc6b130840eb48d829a936837e4ff
Snoweuph <domsaeume@gmail.com>
2022-03-06 22:56:43 +0100

    added css and basic layout, only functonality missing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import './table.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <img src={logo} className="logo" alt="logo" width="125" height="125" />
+        <h1>React.js Todo App</h1>
       </header>
+      <main>
+        <div className="add-task">
+          <input
+            v-model="task"
+            type="text"
+            placeholder="Enter Task"
+            id="add-Task-Input"
+          />
+          <button className="add-task-submit">Submit</button>
+        </div>
+        <table className="task-table">
+          <thead>
+            <tr>
+              <th scope="col">Task</th>
+              <th scope="col" id="table-head-status">Status</th>
+              <th scope="col" className="text-center table-icon">#</th>
+              <th scope="col" className="text-center table-icon">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr >
+              <td></td>
+              <td className="text-center task-status">
+                <select name="task-status" className="task-status-select">
+                  <option>
+                  </option>
+                </select>
+              </td>
+              <td>
+                <div className="text-center task-action-button" >
+                  <span className="fa fa-pen"></span>
+                </div>
+              </td>
+              <td>
+                <div className="text-center task-action-button" >
+                  <span className="fa fa-trash"></span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 ReactDOM.render(
   <React.StrictMode>
